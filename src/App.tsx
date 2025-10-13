107c0902786ce054fd6e4ec5625c22d004fe8b85
Papitas lays sabor original <al.retamalo@duocuc.cl>
2025-10-13 15:13:16 -0300

    Probando commits con bash

--- src/App.tsx
+++ src/App.tsx
@@ -5,35 +5,37 @@ import "./App.css";
 
 function App() {
   return (
-      <div className="app-container">
-        <nav className="navbar navbar-expand-lg navbar-dark bg-primary fixed-top w-100 shadow">
-          <div className="container-fluid">
-            <Link className="navbar-brand fw-bold" to="/">
-              Farmaceutica
-            </Link>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNav">
-                          <ul className="navbar-nav ms-auto">
-              <li className="nav-item">
-                <Link className="nav-link" to="/"> Home</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/productos">
-                  Productos
-                </Link>                
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/home">
-                  Home
-                </Link>
-              </li>
-            </ul> 
+    <>
+      <div>
+        <div className="app-container">
+          <nav className="navbar navbar-expand-lg navbar-dark bg-primary fixed-top w-100 shadow">
+            <div className="container-fluid">
+              <Link className="navbar-brand fw-bold" to="/">
+                Farmaceutica
+              </Link>
+              <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+                <span className="navbar-toggler-icon"></span>
+              </button>
+              <div className="collapse navbar-collapse" id="navbarNav">
+                            <ul className="navbar-nav ms-auto">
+                <li className="nav-item">
+                  <Link className="nav-link" to="/"> Home</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/productos">
+                    Productos
+                  </Link>                
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/home">
+                    Home
+                  </Link>
+                </li>
+              </ul> 
+              </div>
             </div>
-          </div>
-        </nav>
-
+          </nav>
+        </div>
         <div>
           <Routes>
             <Route path="/" element={<Home />} />
@@ -41,12 +43,15 @@ function App() {
             <Route path="/home" element={<Home />} />
           </Routes>
         </div>
-
         <footer className="footer text-center py-3 bg-primary text-white">
             &copy; 2024 Farmaceutica. All rights reserved.
-        </footer>
+        </footer>   
       </div>
+    </>
   )
 }
 
 export default App
+
+
+//Probando commits con bash
